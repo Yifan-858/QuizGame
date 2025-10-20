236b84a44020ad422bf3f4f805d1e5d0bb994089
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2025-10-20 13:56:31 +0200

    Add project files.

---
+++ QuizGame/Models/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Models
+{
+    public class Question
+    {
+        public string Statement { get; }
+        public string[] Answers { get; }
+        public int CorrectAnswer { get; }
+    }
+}
---
+++ QuizGame/Models/Quiz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame.Models
+{
+    public class Quiz
+    {
+        private IEnumerable<Question> _questions;
+        private string _title = string.Empty;
+        public IEnumerable<Question> Questions => _questions;
+        public string Title => _title;
+
+        public Quiz()
+        {
+            _questions = new List<Question>();
+        }
+
+        public Question GetRandomQuestion()
+        {
+            throw new NotImplementedException("A random Question needs to be returned here!");
+        }
+
+        public void AddQuestion(string statement, int correctAnswer, params string[] answers)
+        {
+            throw new NotImplementedException("Question need to be instantiated and added to list of questions here!");
+        }
+
+        public void RemoveQuestion(int index)
+        {
+            throw new NotImplementedException("Question at requested index need to be removed here!");
+        }
+        }
+}
